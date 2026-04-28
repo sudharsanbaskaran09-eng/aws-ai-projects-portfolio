0eb45208980953d060cfc1c8fbacaa79b1036303
Sudharsan B <sudharsanbaskaran09@gmail.com>
2026-04-28 20:10:41 +0530

    Update chatbot_client.py

--- project-4-smart-chatbot/chatbot_client.py
+++ project-4-smart-chatbot/chatbot_client.py
@@ -1,12 +1,1 @@
-import requests
-import uuid
-import json
-        result = response.json()
 
-        # API Gateway usually wraps response in 'body'
-        body = json.loads(result.get('body', '{}'))
-
-        return body.get('bot_response', "No response from bo
-
-    except requests.exceptions.RequestException as e:
-        return f"Error connectin
