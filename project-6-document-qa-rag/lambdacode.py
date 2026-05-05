416fbea1626704920ac158ad20dcc9aa38bfe602
Sudharsan B <sudharsanbaskaran09@gmail.com>
2026-05-05 20:15:05 +0530

    Update lambdacode.py

--- project-6-document-qa-rag/lambdacode.py
+++ project-6-document-qa-rag/lambdacode.py
@@ -3,9 +3,4 @@ import json
     r
     }
 
-    response = bedrock_client.invoke_model')
-
-    if not question:
-        return {
-            'statusCode': 400,
-            'body': json.dumps({'error': 'Question is required'})
+  error': 'Question is required'})
