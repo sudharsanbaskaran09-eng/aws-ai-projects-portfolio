0d8d6fef05e3d5bcf5f68fe5909ce3c9034a2350
Sudharsan B <sudharsanbaskaran09@gmail.com>
2026-05-06 22:21:59 +0530

    Implement lambda function for document QA

--- project-6-document-qa-rag/lambdacode.py
+++ project-6-document-qa-rag/lambdacode.py
@@ -1,1 +1,68 @@
+import boto3
+import json
+
+    response_body = json.loads(response['body'].read())
+    return response_body['output']['message']['content'][0]['text']
+
+def lambda_handler(event, context):
+    if isinstance(event.get('body'), str):
+        body = json.loads(event['body'])
+    else:
+        body = event
+
+    question = body.get('question', '')
+
+    if not question:
+        return {
+            'statusCode': 400,
+            'body': json.dumps({'error': 'Question is required'})
+        }
+
+    knowledge_base = load_knowledge_base()
+    chunks = split_into_chunks(knowledge_base)
+    relevant_chunks = find_relevant_chunks(question, chunks)
+    context_text = '\n\n'.join(relevant_chunks)
+    answer = generate_answer(question, context_text)
+
+    return {
+        'statusCode': 200,
+        'headers': {
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        },
+        'body': json.dumps({
+            'question': question,
+            'answer': answer,
+            'chunks_used': len(relevant_chunks),
+            'model': MODEL_ID
+        })
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
