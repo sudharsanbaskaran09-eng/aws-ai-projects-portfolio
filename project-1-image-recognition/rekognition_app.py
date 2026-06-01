e6cd8d001b7536ec79bb3f4b44e8241102dab8a9
Sudharsan B <sudharsanbaskaran09@gmail.com>
2026-06-01 22:16:20 +0530

    Clean up unused AWS client and upload code
    Removed unused AWS Rekognition client initialization and file upload code.

--- project-1-image-recognition/rekognition_app.py
+++ project-1-image-recognition/rekognition_app.py
@@ -2,14 +2,7 @@ import boto3
 import json
 
 # Initialize AWS clients
-s3_client = boto3.client('s3', region_name='us-east-1')
-rekognition_client = boto3.client('rekognition', region_name='us-east-1')
-
-
-
-    s3_client.upload_file('results.json', BUCKET_NAME, f'results/{result_file}')
-    print(f"☁️ Results saved to S3!")
-
+s3_client
 # ---- MAIN ----
 if __name__ == "__main__":
     IMAGE_PATH = "test_image.jpg"
