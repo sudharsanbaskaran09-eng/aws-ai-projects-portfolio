37b64b85a6ba20f4a7bc372d8009fe2d921e05d1
Sudharsan B <sudharsanbaskaran09@gmail.com>
2026-06-01 22:16:31 +0530

    Update rekognition_app.py

--- project-1-image-recognition/rekognition_app.py
+++ project-1-image-recognition/rekognition_app.py
@@ -3,11 +3,3 @@ import json
 
 # Initialize AWS clients
 s3_client
-# ---- MAIN ----
-if __name__ == "__main__":
-    IMAGE_PATH = "test_image.jpg"
-    IMAGE_NAME = "test_image.jpg"
-
-    upload_image_to_s3(IMAGE_PATH, IMAGE_NAME)
-    analyze_image(IMAGE_NAME)
-    save_results_to_s3(IMAGE_NAME)
