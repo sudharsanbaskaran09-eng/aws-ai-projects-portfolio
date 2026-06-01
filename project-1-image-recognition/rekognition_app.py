dbf200f575a4b6af7dc37a9714f9b5dd96b28965
Sudharsan B <sudharsanbaskaran09@gmail.com>
2026-06-01 22:14:36 +0530

    Update rekognition_app.py

--- project-1-image-recognition/rekognition_app.py
+++ project-1-image-recognition/rekognition_app.py
@@ -6,16 +6,7 @@ s3_client = boto3.client('s3', region_name='us-east-1')
 rekognition_client = boto3.client('rekognition', region_name='us-east-1')
 
 
-def upload_image_to_s3(image_path, image_name):
-    """Upload local image to S3"""
-    print(f"📤 Uploading {image_name} to S3...")
-    s3_client.upload_file(image_path, BUCKET_NAME, f'uploads/{image_name}')
-    print(f"✅ Upload successful!")
 
-def analyze_image(image_name):
-    print(f"🤖 Analyzing i
-
-    # Save results locally
     s3_client.upload_file('results.json', BUCKET_NAME, f'results/{result_file}')
     print(f"☁️ Results saved to S3!")
 
